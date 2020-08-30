a8c58e29201eb6cda54b5a89dcb86123e537409b
Will Crawford <willcrawford@Parkers-MacBook-Air.local>
2020-08-30 15:34:10 -0400

    fixed error page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -43,8 +44,19 @@ app.use('/', usersRouter);
 app.use('/auth', authRouter)
 
 // invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('error');
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
